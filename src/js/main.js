6cba0dfd812738fd113e0c5f0f80871349f94c31
NadiaQN <quezada.nadia@gmail.com>
2018-08-12 15:22:07 -0300

    se imprime el objecto con exito

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,16 @@
 let inNumCard = document.getElementById("textNum");
 btnNum.addEventListener("click", () => {
   inNumCard.value;
-  
+
   //inNumCard = " ";
   let infoTarjeta = {
     cardBip: inNumCard.value
   };
   saveContactTarj(infoTarjeta);
- let createTarjNum = document.getElementById("createTarj");
+
+  let createTarjNum = document.getElementById("createTarj");
   let cardNum = document.createElement("h1");
   createTarjNum.appendChild(cardNum);
-  cardNum.innerHTML = inNumCard;
-  console.log(inNumCard);
+  cardNum.innerHTML = (Object.values(infoTarjeta));
+
 });
