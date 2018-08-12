00d474b9608135d30a66c87e7d39c63f861520df
NadiaQN <quezada.nadia@gmail.com>
2018-08-12 13:46:34 -0300

    save number of card in firebase

--- src/js/app.js
+++ src/js/app.js
@@ -6,40 +6,46 @@
 
 // wiew
 
-
-  firebase.auth().onAuthStateChanged(user => {
-    if (user) {
-      let emailUser = user.email;
-      printUser.innerHTML = emailUser;
-      //Si estamos logueados
-      firstView.style.display = "none";
-      secondView.style.display = "block";
-      console.log("User > " + JSON.stringify(user));
-    } else {
-      //No estamos logueados
-      firstView.style.display = "block";
-      secondView.style.display = "none";
-    }
-  });
-
-  function starIn() {
-    let email = document.getElementById("inputEmail").value;
-    let password = document.getElementById("inputPassword").value;
-    console.log(email);
-
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(() => {
-        console.log("Usuario creado");
-      })
-      .catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // ...
-        console.log(errorCode);
-        console.log(errorMessage);
-      });
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    let emailUser = user.email;
+    printUser.innerHTML = emailUser;
+    //Si estamos logueados
+    firstView.style.display = "none";
+    secondView.style.display = "block";
+    console.log("User > " + JSON.stringify(user));
+  } else {
+    //No estamos logueados
+    firstView.style.display = "block";
+    secondView.style.display = "none";
   }
+});
+
+function starIn() {
+  let email = document.getElementById("inputEmail").value;
+  let password = document.getElementById("inputPassword").value;
+  console.log(email);
+
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .then(() => {
+      console.log("Usuario creado");
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      console.log(errorCode);
+      console.log(errorMessage);
+    });
+}
+
+function saveContactTarj(infoTarjeta) {
+firebase.database().ref("tarjeta").push(infoTarjeta) //
+.then(function() {
+});
+}
+
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,16 @@
 // AQUI VA EL DOM
-
+let inNumCard = document.getElementById("textNum");
 btnNum.addEventListener("click", () => {
-  let inNumCard = document.getElementById("textNum").value;
+  inNumCard.value;
+  
   //inNumCard = " ";
-  let createTarjNum = document.getElementById("createTarj");
+  let infoTarjeta = {
+    cardBip: inNumCard.value
+  };
+  saveContactTarj(infoTarjeta);
+ let createTarjNum = document.getElementById("createTarj");
   let cardNum = document.createElement("h1");
   createTarjNum.appendChild(cardNum);
   cardNum.innerHTML = inNumCard;
+  console.log(inNumCard);
 });
